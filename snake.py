767b163ab797aa34c7022dae4e46430317b2329e
Matthieu Lamarle <matthieu.lamarle@etu.minesparis.psl.eu>
2022-11-09 17:02:41 +0100

    structuration en fonction

--- snake.py
+++ snake.py
@@ -2,16 +2,12 @@ import sys
 import pygame
 import random
 
-#Setup
-pygame.init()
-screen = pygame.display.set_mode([600, 600])
-clock = pygame.time.Clock()
-
 #Constant
+
 x0 = 0
 y0 = 0
-width = 20
-height = 20
+width = 30
+height = 30
 cell_size = 20
 Blanc = [255,255,255]
 Noir = [0,0,0]
@@ -24,22 +20,29 @@ DOWN = [0.0, 1.0]
 RIGHT = [1.0, 0.0]
 
 # Game State
+
 snake = [[10, 15],[11, 15],[12, 15],]
 direction = [1,0]
 fruit = [10,10]
 score = 0
 
+# Helper Functions
+
+def setup():
+    #Setup
+    width_height = [width*cell_size, height*cell_size]
+    pygame.init()
+    screen = pygame.display.set_mode(width_height)
+    clock = pygame.time.Clock()
+    return screen, clock
+
 def exit():
     pygame.quit()
     sys.exit()
 
-
-
-
-#Main Loop
-while True :
-
+def handle_events():
     # Event Management
+    global direction
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
@@ -55,9 +58,10 @@ while True :
             elif event.key == pygame.K_RIGHT:
                 direction = RIGHT
 
+def move_snake():
     #Game Logic (move snake)
+    global fruit, score, snake
     head = snake[-1]
-    tail = snake[0]
     new_head = [head[0] + direction[0], head[1] + direction[1]]
     if (new_head in snake or new_head[0] < 0 or new_head[0] >= 30 or new_head[1] < 0 or new_head[1] >= 30):
         exit()
@@ -68,16 +72,25 @@ while True :
     else:
         snake = snake[1:] + [new_head]
 
+def draw_frame(screen):
     #Frame Update
     screen.fill(colors["background"])
     for k in range(len(snake)) :
-        rect = [snake[k][0]*cell_size, snake[k][1]*cell_size, width, height]
+        rect = [snake[k][0]*cell_size, snake[k][1]*cell_size, cell_size, cell_size]
         pygame.draw.rect(screen, colors["snake"], rect)
-    pygame.draw.rect(screen, colors["fruit"], [fruit[0]*cell_size, fruit[1]*cell_size, width, height])
+    pygame.draw.rect(screen, colors["fruit"], [fruit[0]*cell_size, fruit[1]*cell_size, cell_size, cell_size])
     pygame.display.update()
     pygame.display.set_caption(f"🐍 Score: {score}")
-
+    
+def wait_for_next_frame(clock):
     #Wait for next frame
     clock.tick(FPS)
 
-    
+#Setup & Main Loop
+
+screen, clock = setup()
+while True :
+    handle_events()
+    move_snake()
+    draw_frame(screen)
+    wait_for_next_frame(clock)
