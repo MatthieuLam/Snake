4bd9001218d2f7d5cba6c6dbcbf64f7749b92019
Matthieu Lamarle <matthieu.lamarle@etu.minesparis.psl.eu>
2022-11-09 16:16:17 +0100

    commentaires + constantes

--- snake.py
+++ snake.py
@@ -2,58 +2,82 @@ import sys
 import pygame
 import random
 
+#Setup
 pygame.init()
 screen = pygame.display.set_mode([600, 600])
 clock = pygame.time.Clock()
 
+#Constant
 x0 = 0
 y0 = 0
 width = 20
 height = 20
-B = [255,255,255]
-N = [0,0,0]
-R = [255, 0, 0]
+cell_size = 20
+Blanc = [255,255,255]
+Noir = [0,0,0]
+Rouge = [255, 0, 0]
+FPS = 3
+colors = {"background": Blanc, "snake": Noir, "fruit": Rouge}
+UP = [0.0, -1.0]
+LEFT = [-1.0, 0.0]
+DOWN = [0.0, 1.0]
+RIGHT = [1.0, 0.0]
+
+# Game State
 snake = [[10, 15],[11, 15],[12, 15],]
 direction = [1,0]
 fruit = [10,10]
 score = 0
 
+def exit():
+    pygame.quit()
+    sys.exit()
+
+
+
+
+#Main Loop
 while True :
+
+    # Event Management
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q :
-                pygame.quit()
-                sys.exit()
+                exit()
             if event.key == pygame.K_UP:
-                direction = [0.0, -1.0]
+                direction = UP
             elif event.key == pygame.K_LEFT:
-                direction = [-1.0, 0.0]
+                direction = LEFT
             elif event.key == pygame.K_DOWN:
-                direction = [0.0, 1.0]
+                direction = DOWN
             elif event.key == pygame.K_RIGHT:
-                direction = [1.0, 0.0]
+                direction = RIGHT
+
+    #Game Logic (move snake)
     head = snake[-1]
     tail = snake[0]
     new_head = [head[0] + direction[0], head[1] + direction[1]]
     if (new_head in snake or new_head[0] < 0 or new_head[0] >= 30 or new_head[1] < 0 or new_head[1] >= 30):
-        pygame.quit()
-        sys.quit
+        exit()
     elif new_head == fruit :
         score += 1
         snake = snake + [new_head]
         fruit = [random.randint(0, 29), random.randint(0, 29)]
     else:
         snake = snake[1:] + [new_head]
-    screen.fill(B)
+
+    #Frame Update
+    screen.fill(colors["background"])
     for k in range(len(snake)) :
-        rect = [snake[k][0]*20, snake[k][1]*20, width, height]
-        pygame.draw.rect(screen, N , rect)
-    pygame.draw.rect(screen, R, [fruit[0]*20, fruit[1]*20, width, height])
+        rect = [snake[k][0]*cell_size, snake[k][1]*cell_size, width, height]
+        pygame.draw.rect(screen, colors["snake"], rect)
+    pygame.draw.rect(screen, colors["fruit"], [fruit[0]*cell_size, fruit[1]*cell_size, width, height])
     pygame.display.update()
     pygame.display.set_caption(f"🐍 Score: {score}")
-    clock.tick(3)
+
+    #Wait for next frame
+    clock.tick(FPS)
 
     
